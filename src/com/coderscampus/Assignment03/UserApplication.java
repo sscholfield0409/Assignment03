9f072f32733d724f3dcac2f0f5b8160f5cf4970f
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-06 12:35:42 -0500

    Update UserApplication
    consolidating code

--- src/com/coderscampus/Assignment03/UserApplication.java
+++ src/com/coderscampus/Assignment03/UserApplication.java
@@ -11,9 +11,6 @@ public class UserApplication {
 			users[i] = userService.createUser("user" + (i + 1), "password" + (i + 1), "name" + (i + 1));
 		}
 		
-		users[0] = userService.createUser("test@email.com", "passwordTest!1", "Test User");
-		users[1] = userService.createUser("another@user.com", "another@user.com", "Another User");
-		users[2] = userService.createUser("john.doe@mydomain.net", "Hdk398jf!", "John Doe");
-		users[3] = userService.createUser("jane.doe@myotherdomain.com", "sunrise-sunset", "Jane Doe");
+		System.out.println(users);
 	}
 }
