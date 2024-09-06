ec3660a3ed5eaf55ba5a743813116e821adeae12
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-06 12:58:01 -0500

    Update UserService.java
    adjusting user information in UserService

--- src/com/coderscampus/Assignment03/UserService.java
+++ src/com/coderscampus/Assignment03/UserService.java
@@ -3,9 +3,9 @@ package com.coderscampus.Assignment03;
 public class UserService {
 	public User createUser (String username, String password, String name) {
 		User user = new User();
-		user.setUsername(username);
-		user.setPassword(password);
-		user.setName(name);
+		user.setUsername("test@email.com");
+		user.setPassword("passwordTest!1");
+		user.setName("Test User");
 		return user;
 	}
 }
