68ab04fa6a79bc8cb454a28ff248cf1dd156cce0
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-06 12:34:36 -0500

    Update UserApplication.java
    including for loop into the userApplication to better store the user[]

--- src/com/coderscampus/Assignment03/UserApplication.java
+++ src/com/coderscampus/Assignment03/UserApplication.java
@@ -7,6 +7,10 @@ public class UserApplication {
 		
 		User[] users = new User[4];
 		
+		for (int i = 0; i < 4; i++) {
+			users[i] = userService.createUser("user" + (i + 1), "password" + (i + 1), "name" + (i + 1));
+		}
+		
 		users[0] = userService.createUser("test@email.com", "passwordTest!1", "Test User");
 		users[1] = userService.createUser("another@user.com", "another@user.com", "Another User");
 		users[2] = userService.createUser("john.doe@mydomain.net", "Hdk398jf!", "John Doe");
