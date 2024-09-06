3d7bff7d42a039e00db36cf75396851ed59e144d
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-06 12:29:23 -0500

    update UserApplication.java
    sending commit with updated information

--- src/com/coderscampus/Assignment03/UserApplication.java
+++ src/com/coderscampus/Assignment03/UserApplication.java
@@ -7,9 +7,9 @@ public class UserApplication {
 		
 		User[] users = new User[4];
 		
-		users[0] = new User("test@email.com", "passwordTest!1", "Test User");
-		users[1] = new User("another@user.com", "another@user.com", "Another User");
-		users[2] = new User("john.doe@mydomain.net", "Hdk398jf!", "John Doe");
-		users[3] = new User("jane.doe@myotherdomain.com", "sunrise-sunset", "Jane Doe");
+		users[0] = userService.createUser("test@email.com", "passwordTest!1", "Test User");
+		users[1] = userService.createUser("another@user.com", "another@user.com", "Another User");
+		users[2] = userService.createUser("john.doe@mydomain.net", "Hdk398jf!", "John Doe");
+		users[3] = userService.createUser("jane.doe@myotherdomain.com", "sunrise-sunset", "Jane Doe");
 	}
 }
