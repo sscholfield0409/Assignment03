39c32d60dbbe862ec900a45389e2e5fb27b2a566
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-06 12:27:37 -0500

    Update UserApplication.java
    Trying to follow videos before officiating asssignment. sending commit so I don't lose my spot

--- src/com/coderscampus/Assignment03/UserApplication.java
+++ src/com/coderscampus/Assignment03/UserApplication.java
@@ -5,8 +5,11 @@ public class UserApplication {
 	public static void main(String[] args) {
 		UserService userService = new UserService();
 		
-		User testUser = userService.createUser("test@email.com", "passwordTest!1", "Test User");
-		System.out.println(testUser);
+		User[] users = new User[4];
+		
+		users[0] = new User("test@email.com", "passwordTest!1", "Test User");
+		users[1] = new User("another@user.com", "another@user.com", "Another User");
+		users[2] = new User("john.doe@mydomain.net", "Hdk398jf!", "John Doe");
+		users[3] = new User("jane.doe@myotherdomain.com", "sunrise-sunset", "Jane Doe");
 	}
-
 }
