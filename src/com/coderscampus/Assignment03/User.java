5464f0695851b8d38db02c8fbfa4c1859cc5dbd4
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-06 08:39:27 -0500

    Update User.java
    providing setters for User.java

--- src/com/coderscampus/Assignment03/User.java
+++ src/com/coderscampus/Assignment03/User.java
@@ -4,6 +4,13 @@ public class User {
 	private String username;
 	private String password;
 	private String name;
+	
+	public User(String username, String password, String name) {
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
+	
 	public String getUsername() {
 		return username;
 	}
