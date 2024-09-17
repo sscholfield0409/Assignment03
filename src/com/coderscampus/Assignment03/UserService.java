d000e5ca3b908d6123db69667c51493e3529f258
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-09-17 13:10:06 -0500

    Update UserService
    updating userService by adding return value

--- src/com/coderscampus/Assignment03/UserService.java
+++ src/com/coderscampus/Assignment03/UserService.java
@@ -4,7 +4,9 @@ public class UserService {
 	public User createUser (String username, String password, String name) {
 		User[] users = new User[4];
 		
+	String input = (String) "username";
 	
+	return null;
 		
 	}
 
